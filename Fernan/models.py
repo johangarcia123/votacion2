85255a97fb22e3b3bd0fbf2af59d33dd1a6726fc
johangarcia123 <johanngarciavizuete@gmail.com>
2022-08-04 09:57:40 -0500

    clases

--- Fernan/models.py
+++ Fernan/models.py
@@ -5,3 +5,19 @@ from django.db import models
 class Fernan(models.Model): 
     title = models.CharField(max_length=100) 
     description = models.TextField(blank=True)
+    
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=100)
+    email = models.EmailField()
+    telefono = models.CharField(max_length=7)
+    
+class Articulo(models.Model):
+    descripcion = models.CharField(max_length=20)
+    precio = models.IntegerField()
+    nombre = models.CharField(max_length=20)
+
+class Pedidos(models.Model):
+    numero     = models.IntegerField()
+    fecha      = models.DateField()
+    entregado  = models.BooleanField()
